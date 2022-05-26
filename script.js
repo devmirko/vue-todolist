43e539fd9a8966ee113179ccaacaa1e75065417c
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-26 15:35:23 +0200

    creazione array di oggetti

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 const toDoList = new Vue ({
-    
+    el : "app",
+    data : {
+        toDo : [
+            {
+                text : 'fare la spesa',
+                done : true
+            },
+            {
+                text : 'fare le pulizie',
+                done : false
+            },
+            {
+                text : 'cucinare',
+                done : false
+            },
+            {
+                text : 'studiare',
+                done : true
+            },
+            {
+                text : 'pagare le bollette',
+                done : false
+            },
+        ]
+    }
 })
